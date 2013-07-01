06c2fef1cf53e7178f887b85c9c2d0d6a7f6f319
Adam Ginsburg <keflavich@gmail.com>
2013-07-01 15:43:24 -0600

    rough radex function written...

--- radex/core.py
+++ radex/core.py
@@ -1,2 +1,51 @@
-def radex(inputs):
-    pass
+import subprocess
+import tempfile
+
+def radex(inputs, executable='radex'):
+
+    infile,outfile = write_input(*args)
+
+    call_radex(executable, outfile)
+
+def write_input(tkin=10, column_density=1e12, collider_densities={'H2':1}, 
+        bw=0.01, tbg=2.73, molecule='co', velocity_gradient=1.0):
+    """
+    Write radex.inp file parameters
+
+    Parameters
+    ----------
+    tkin : float
+        Kinetic temperature (K)
+    collider_densities : dict
+        Collider names and their number densities
+    column_density : float
+        column density of the molecule
+    molecule : str
+        Name of the molecule (specifically, the prefix for the file name, e.g.
+        for "co.dat", molecule='co').  Case sensitive!
+    tbg : float
+        Temperature of the background radiation (e.g. CMB)
+    velocity_gradient : float
+        Velocity gradient per pc in km/s
+    """
+    infile = tempfile.NamedTemporaryFile(mode='w', delete=True)
+    outfile = tempfile.NamedTemporaryFile(mode='w', delete=True)
+    infile.write(molecule+'.dat\n')
+    infile.write(outfile.name+'\n')
+    infile.write(str(flow*(1-bw))+' '+str(fupp/(1-bw))+'\n')
+    infile.write(str(tkin)+'\n')
+    infile.write('%s\n' % len(colliders))
+    for name,dens in colliders.iteritems():
+        infile.write('%s\n' % name)
+        infile.write(str(dens)+'\n')
+    infile.write(str(tbg)+'\n')
+    infile.write(str(column_density)+'\n')
+    infile.write(str(velocity_gradient)+'\n')
+    # end the input file
+    infile.write('0\n')
+    infile.flush()
+    return infile,outfile
+
+def call_radex(executable):
+
+    os.system('%s < radex.inp > /dev/null' % executable)
--- radex/read_radex.py
+++ radex/read_radex.py
@@ -1,65 +1,16 @@
-"""
-This code is exclusively for use of testing read_radex.  Don't use it in any
-production anything until you're sure it works.
-"""
-
-bw    = 0.01   # "bandwidth": free spectral range around line (used to say which lines get printer)
-
-def read_radex_old(outfile,flow,fupp,bw=bw):
-    """
-    A hack-ey means of reading a radex.out file.  
-    Cycles through it based on fixed format
-    """
-    line  = outfile.readline()
-    words = line.split()
-    while (words[-1] != "FLUX"):
-        line  = outfile.readline()
-        words = line.split()
-        if words[1] == "T(kin)":
-            temp  = float(words[-1])
-        if words[1] == "Density" and words[3] == "H2":
-            dens  = float(words[-1])
-        if words[1] == "Column":
-            col  = float(words[-1])
-    line  = outfile.readline()
-    line  = outfile.readline()
-    words = line.split()
-    ftmp  = float(words[4])
-    while ((ftmp < flow*(1-bw)) or (ftmp > flow/(1-bw))):
-        line  = outfile.readline()
-        words = line.split()
-        ftmp  = float(words[4])
-    low   = float(words[-2])
-    TexLow   = float(words[6])
-    TauLow   = float(words[7])
-    TrotLow  = float(words[8])
-    FluxLow  = float(words[11])
-    line  = outfile.readline()
-    words = line.split()
-    ftmp  = float(words[4])
-    while ((ftmp < fupp*(1-bw)) or (ftmp > fupp/(1-bw))):
-        line  = outfile.readline()
-        words = line.split()
-        ftmp  = float(words[4])
-    upp   = float(words[-2])
-    TexUpp   = float(words[6])
-    TauUpp   = float(words[7])
-    TrotUpp  = float(words[8])
-    FluxUpp  = float(words[11])
-    return temp,dens,col,TexLow,TexUpp,TauLow,TauUpp,TrotLow,TrotUpp,FluxLow,FluxUpp
-
 def tryfloat(x):
     try:
         return float(x)
     except ValueError:
         return float(0)
 
-def read_radex(file,flow,fupp,bw=bw,debug=False):
+def read_radex(file,flow,fupp,bw=0.01,debug=False):
     """ 
     less hack-ey way to read radex.out files
     """
     linenum = 0
-    line = file.readline(); linenum+=1
+    line = file.readline()
+    linenum+=1
     if debug: print line
     if line == '':
         return 0
