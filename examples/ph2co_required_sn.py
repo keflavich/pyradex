b8211b87034b570ee0f18c59ced3d0e53455222d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-11-26 11:41:19 +0100

    updated ph2co

--- examples/ph2co_required_sn.py
+++ examples/ph2co_required_sn.py
@@ -7,14 +7,32 @@ import pylab as pl
 import numpy as np
 import matplotlib
 import astropy.units as u
+import os
+
+if os.path.exists('/Users/adam/.matplotlib/ggplotrc'):
+    matplotlib.rc_file('/Users/adam/.matplotlib/ggplotrc')
+
+def latex_float(f):
+    float_str = "{0:.1g}".format(f)
+    if "e" in float_str:
+        base, exponent = float_str.split("e")
+        return r"{0} \times 10^{{{1}}}".format(base, int(exponent))
+    else:
+        return float_str
 
 ntemp = 50
 temperatures = np.linspace(10,200,ntemp)
 abundance = 10**-8.5
 
 density = 1e4
+nh2 = 1e22
 
-R = pyradex.Radex(species='ph2co-h2', abundance=abundance, collider_densities={'H2':density}, column=None, temperature=temperatures[0])
+R = pyradex.Radex(species='ph2co-h2',
+                  abundance=abundance,
+                  collider_densities={'H2':density},
+                  column=None,
+                  temperature=temperatures[0],
+                  h2column=nh2)
 
 pl.figure(1)
 pl.clf()
@@ -28,45 +46,61 @@ for temperature in [10,50,100]:
 pl.legend(loc='best')
 
 # create a small grid...
-ratio = []
-f1 = []
-f2 = []
+densities = [10**x for x in xrange(2,7)]
+ratio = {d:[] for d in densities}
+f1 = {d:[] for d in densities}
+f2 = {d:[] for d in densities}
 
-for density in [100,1e4,1e6]:
+for density in densities:
     R.density = {'H2': density}
     for temperature in temperatures:
         R.temperature = temperature
-        R.run_radex()
+        print R.run_radex(),
 
         F1 = R.T_B[2]
         F2 = R.T_B[12]
 
-        ratio.append(F2/F1)
-        f2.append(F2)
-        f1.append(F1)
+        ratio[density].append(F2/F1)
+        f2[density].append(F2)
+        f1[density].append(F1)
+    print
 
-ratio = np.array(ratio)
+f1 = {d:np.array([x.value for x in f1[d]]) for d in densities}
+f2 = {d:np.array([x.value for x in f2[d]]) for d in densities}
+ratio = {d:np.array(ratio[d]) for d in densities}
 
 pl.figure(2)
 pl.clf()
 
-pl.plot(ratio[:50]   ,temperatures)
-pl.plot(ratio[50:100],temperatures)
-pl.plot(ratio[100:]  ,temperatures)
+for d in densities:
+    pl.plot(ratio[d],temperatures,label='$n=10^{%i}$' % (np.log10(d)))
+
+    m = 1/((ratio[d][15]-ratio[d][5])/(temperatures[15]-temperatures[5]))
+    b = temperatures[5]-ratio[d][5]*m
+    line=(m,b)
+    print d,m,b
+    pl.plot(ratio[d],ratio[d]*line[0]+line[1],'--')
+
 pl.ylabel("Temperature")
 pl.xlabel("$S(3_{2,1}-2_{2,0})/S(3_{0,3}-2_{0,2})$")
+pl.legend(loc='best',fontsize=14)
+pl.title("$N(H_2) = %s$ cm$^{-2}$, X(p-H$_2$CO)$=10^{%0.1f}$" % (latex_float(nh2),np.log10(abundance)))
 
-m = 1/((ratio[15]-ratio[5])/(temperatures[15]-temperatures[5]))
-b = temperatures[5]-ratio[5]*m
-line=(m,b)
-pl.plot(ratio,ratio*line[0]+line[1])
 pl.axis([0,0.5,10,200,])
 
-f1 = np.array([x.value for x in f1])
-f2 = np.array([x.value for x in f2])
-pl.plot(f2[:50]   ,temperatures)
-pl.plot(f2[50:100],temperatures)
-pl.plot(f2[100:]  ,temperatures)
-pl.plot(f1[:50]   ,temperatures,'--')
-pl.plot(f1[50:100],temperatures,'--')
-pl.plot(f1[100:]  ,temperatures,'--')
+pl.savefig("pH2CO_ratio_vs_temperature.pdf",bbox_inches='tight')
+
+pl.figure(3)
+pl.clf()
+for d in densities:
+    pl.plot(temperatures,f2[d],label='$n=10^{%i}$' % (np.log10(d)))
+pl.xlabel("Temperature")
+pl.ylabel("$T_B(3_{2,1}-2_{2,0})$")
+ax = pl.gca()
+pl.plot(ax.get_xlim(),[1.5,1.5],'k--',label='S/N$=5$, $\sigma=0.3$ K')
+pl.plot(ax.get_xlim(),[2.5,2.5],'k:',label='S/N$=5$, $\sigma=0.5$ K')
+pl.plot(ax.get_xlim(),[0.25,0.25],'k-.',label='S/N$=5$, $\sigma=0.05$ K')
+ax.axis([0,100,0,3.2])
+pl.legend(loc='best',fontsize=14)
+pl.title("$N(H_2) = %s$ cm$^{-2}$, X(p-H$_2$CO)$=10^{%0.1f}$" % (latex_float(nh2),np.log10(abundance)))
+pl.savefig("pH2CO_321-220_vs_temperature.pdf",bbox_inches='tight')
