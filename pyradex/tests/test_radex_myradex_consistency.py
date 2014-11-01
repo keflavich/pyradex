d0db07ff6376ff978089a0c84601c06747f206e6
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-11-01 19:08:08 +0100

    add datapath in tests

--- pyradex/tests/test_radex_myradex_consistency.py
+++ pyradex/tests/test_radex_myradex_consistency.py
@@ -14,8 +14,21 @@ def test_thin_co():
     density = {'oH2': 100,
                'pH2': 900,
               }
-    RR = pyradex.Radex(species='co', column=1e10, density=density, temperature=20)
-    FF = pyradex.fjdu.Fjdu(species='co', column=1e10, density=density, temperature=20)
+    RR = pyradex.Radex(datapath='examples/', species='co', column=1e10, density=density, temperature=20)
+    FF = pyradex.fjdu.Fjdu(datapath='examples/', species='co', column=1e10, density=density, temperature=20)
+
+    rtbl = RR()
+    ftbl = FF()
+
+    diff = rtbl['upperlevelpop'] - ftbl['upperlevelpop']
+    #np.testing.assert_allclose(diff, 0)
+
+def test_thick_co():
+    density = {'oH2': 100,
+               'pH2': 900,
+              }
+    RR = pyradex.Radex(datapath='examples/', species='co', column=1e17, density=density, temperature=20)
+    FF = pyradex.fjdu.Fjdu(datapath='examples/', species='co', column=1e17, density=density, temperature=20)
 
     rtbl = RR()
     ftbl = FF()
