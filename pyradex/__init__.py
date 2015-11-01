d6560eafdd53975b3bc99518d24bc497c9a0581c
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-11-01 10:35:29 +0100

    revert to my own setup because it's highly customized

--- pyradex/__init__.py
+++ pyradex/__init__.py
@@ -1,21 +1,8 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from .version import __version__
 
-"""
-This is an Astropy affiliated package.
-"""
+from .core import pyradex,write_input,parse_outfile,call_radex,Radex
 
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# not sure if this is needed: it's not a normal part of package-template from .version import __version__
-# ----------------------------------------------------------------------------
-
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    from .core import pyradex,write_input,parse_outfile,call_radex,Radex
-
-    from . import utils
-    from . import despotic_interface
-    from . import radex
-    from . import synthspec
+from . import utils
+from . import despotic_interface
+from . import radex
+from . import synthspec
